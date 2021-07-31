802677f8bedbd91ecc7d5ac704010cf396ea5081
Martijn <m_kopie@hotmail.com>
2021-07-31 22:40:32 +0200

    Copyright

--- simplegl/include/simplegl/pointset.h
+++ simplegl/include/simplegl/pointset.h
@@ -1,3 +1,16 @@
+/*=========================================================================
+
+  Program:        Simple Graphics Library
+  Author:         Martijn Koopman
+  Co-author(s):
+  License:        GNU General Public License Version 3
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+
+=========================================================================*/
+
 #ifndef SIMPLEGL_POINTSET_H
 #define SIMPLEGL_POINTSET_H
 
--- simplegl/src/pointset.cpp
+++ simplegl/src/pointset.cpp
@@ -1,4 +1,15 @@
+/*=========================================================================
 
+  Program:        Simple Graphics Library
+  Author:         Martijn Koopman
+  Co-author(s):
+  License:        GNU General Public License Version 3
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.
+
+=========================================================================*/
 
 #include "simplegl/pointset.h"
 
